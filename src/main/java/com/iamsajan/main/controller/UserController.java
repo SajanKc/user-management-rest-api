40ea2aaa0983f0e7c49c3016de27b63422f5b195
Sajan Kc <sajan1996kc@gmail.com>
2022-01-18 19:26:22 +0545

    api documentation and actuator added

--- src/main/java/com/iamsajan/main/controller/UserController.java
+++ src/main/java/com/iamsajan/main/controller/UserController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.iamsajan.main.model.User;
 import com.iamsajan.main.services.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 /**
  * @Author Sajan K.C.
  * @Date January 7, 2022
@@ -31,21 +33,25 @@ public class UserController {
 	private UserService userService;
 
 	@GetMapping("/users")
+	@Operation(summary = "Get all users", description = "Get all users from Database.")
 	public ResponseEntity<List<User>> getUsers() {
 		return userService.getUsers();
 	}
 
 	@GetMapping("/users/{userId}")
+	@Operation(summary = "Get user", description = "Get user by id from Database.")
 	public ResponseEntity<User> getUsersById(@PathVariable String userId) {
 		return userService.getUsersById(Integer.parseInt(userId));
 	}
 
 	@PostMapping("/users")
+	@Operation(summary = "Add user", description = "Add user to Database.")
 	public ResponseEntity<HttpStatus> addUsers(@RequestBody User user) {
 		return userService.addUsers(user);
 	}
 
 	@PutMapping("/users/{userId}")
+	@Operation(summary = "Update user", description = "Update user by id from Database.")
 	public ResponseEntity<HttpStatus> updateUsers(@PathVariable("userId") Integer userId,
 			@RequestParam(required = false) String fullName, @RequestParam(required = false) String address,
 			@RequestParam(required = false) String email, @RequestParam(required = false) LocalDate dob) {
@@ -53,6 +59,7 @@ public class UserController {
 	}
 
 	@DeleteMapping("/users/{userId}")
+	@Operation(summary = "Delete user", description = "Delete user by id from Database.")
 	public void deleteUsersById(@PathVariable Integer userId) {
 		userService.deleteUsersById(userId);
 	}
